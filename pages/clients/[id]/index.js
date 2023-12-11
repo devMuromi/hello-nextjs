7f1b7b4c466204f62c32996853e73879a6bbcdf8
devMuromi <devmuromi@gmail.com>
2023-12-11 20:35:23 +0900

    router.push, router.replace

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,20 @@
+import { useRouter } from "next/router";
+
 function ClientsProjectPage() {
+  const router = useRouter();
+  function loadProjectHandler() {
+    // router.push("/clients/max/projecta");
+    // router.replace("/clients/max/projecta"); // replace the current page(cannot go back)
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "projecta" },
+    });
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
