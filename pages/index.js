649475bf92cc56d31704210fe659157cb6943f98
devMuromi <devmuromi@gmail.com>
2023-12-12 16:40:36 +0900

    next do pre-rendering by default

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+function HomePage(props) {
+  // Next pre-render every page that has no dynamic data
+  return (
+    <ul>
+      <li>Product 1</li>
+      <li>Product 2</li>
+      <li>Product 3</li>
+    </ul>
+  );
 }
 
 export default HomePage;
