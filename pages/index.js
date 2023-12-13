e756d0cde28b0567cf3a18d0cdd086ed0a441d14
devMuromi <devmuromi@gmail.com>
2023-12-13 17:00:34 +0900

    add server-side function

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,25 @@
 function HomePage(props) {
   // Next pre-render every page that has no dynamic data
+  const { products } = props;
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+}
+
+// this function will run on the server and not shown on the client
+export async function getStaticProps(context) {
+  return {
+    props: {
+      products: [
+        { id: "p1", title: "Product 1" },
+        { id: "p2", title: "Product 2" },
+      ],
+    },
+  };
 }
 
 export default HomePage;
