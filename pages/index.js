0b808ceb408882028aba34face510be5ec53842d
devMuromi <devmuromi@gmail.com>
2023-12-17 16:34:35 +0900

    section8 init

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
+import NewsletterRegistration from "@/components/input/newsletter-registration";
 
 function HomePage(props) {
   return (
@@ -11,6 +12,7 @@ function HomePage(props) {
         <title>NextJS Events</title>
         <meta name="description" content="Find a lot of great events that allow you to evolve..." />
       </Head>
+      <NewsletterRegistration />
       <EventList items={props.events} />
     </div>
   );
