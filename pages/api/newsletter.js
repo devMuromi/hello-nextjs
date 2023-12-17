44af45327d21b65c8a560623095d385dd3152b6f
devMuromi <devmuromi@gmail.com>
2023-12-17 16:48:20 +0900

    add newsletter route

---
+++ pages/api/newsletter.js
@@ -0,0 +1,13 @@
+function handler(req, res) {
+  if (req.method === "POST") {
+    const email = req.body.email;
+    if (!email || !email.includes("@")) {
+      res.status(422).json({ message: "Invalid email address." });
+      return;
+    }
+    console.log(email);
+    res.status(201).json({ message: "Signed up!" });
+  }
+}
+
+export default handler;
