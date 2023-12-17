6174e0c5b5f72ef60aadf448f1a0a742c1379209
devMuromi <devmuromi@gmail.com>
2023-12-18 04:42:42 +0900

    context: make alarm

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,11 +1,12 @@
 function handler(req, res) {
   if (req.method === "POST") {
-    const email = req.body.email;
-    if (!email || !email.includes("@")) {
+    const userEmail = req.body.email;
+    if (!userEmail || !userEmail.includes("@")) {
       res.status(422).json({ message: "Invalid email address." });
       return;
     }
-    console.log(email);
+
+    console.log(userEmail);
     res.status(201).json({ message: "Signed up!" });
   }
 }
