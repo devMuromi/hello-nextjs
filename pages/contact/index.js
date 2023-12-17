9510a2e3109446af93c95f11273a45ef7ad4cd62
devMuromi <devmuromi@gmail.com>
2023-12-18 04:59:37 +0900

    init

---
+++ pages/contact/index.js
@@ -0,0 +1,5 @@
+function ContactPage() {
+  return <div>Welcome to Next.js!</div>;
+}
+
+export default ContactPage;
---
+++ pages/posts/[slug].js
@@ -0,0 +1,5 @@
+function PostDetailPage() {
+  return <h1>Post</h1>;
+}
+
+export default PostDetailPage;
