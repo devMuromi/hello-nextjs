771196aa4701f935bfb77926ec0987fb2f0850b8
devMuromi <devmuromi@gmail.com>
2023-12-18 17:15:56 +0900

    add FeaturedPosts

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import { Fragment } from "react";
 
 import Hero from "../components/home-page/hero";
+import FeaturedPosts from "../components/home-page/featured-posts";
+
+const DUMMY_POSTS = [
+  { slug: "getting-started-with-nextjs", title: "Getting Started with NextJS", image: "getting-started-nextjs.png", excerpt: "NextJS is a React Framework for Production", date: "2023-12-18" },
+  { slug: "getting-started-with-nextjs2", title: "Getting Started with NextJS", image: "getting-started-nextjs.png", excerpt: "NextJS is a React Framework for Production", date: "2023-12-18" },
+  { slug: "getting-started-with-nextjs3", title: "Getting Started with NextJS", image: "getting-started-nextjs.png", excerpt: "NextJS is a React Framework for Production", date: "2023-12-18" },
+  { slug: "getting-started-with-nextjs4", title: "Getting Started with NextJS", image: "getting-started-nextjs.png", excerpt: "NextJS is a React Framework for Production", date: "2023-12-18" },
+];
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
-      {/* <FeaturedPosts /> */}
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
