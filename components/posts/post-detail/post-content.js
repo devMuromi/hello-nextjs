3068503a95f4979142f93f078d0b58fcb189fed9
devMuromi <devmuromi@gmail.com>
2023-12-19 13:10:56 +0900

    using <ReactMarkdown/>

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,27 @@
+import ReactMarkdown from "react-markdown";
+
+import classes from "./post-content.module.css";
+
+import PostHeader from "./post-header";
+
+const DUMMY_POST = {
+  slug: "getting-started-with-nextjs",
+  title: "Getting Started with NextJS",
+  image: "getting-started-nextjs.png",
+  excerpt: "NextJS is a React Framework for Production",
+  date: "2023-12-18",
+  content: "# This is a first post",
+};
+
+function PostContent() {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+    </article>
+  );
+}
+
+export default PostContent;
