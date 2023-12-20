1629be4c57b88aa410fe9e23049452874dc37064
devMuromi <devmuromi@gmail.com>
2023-12-20 12:32:39 +0900

    add post detail page: dynamic routing, ssg

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -4,22 +4,14 @@ import classes from "./post-content.module.css";
 
 import PostHeader from "./post-header";
 
-const DUMMY_POST = {
-  slug: "getting-started-with-nextjs",
-  title: "Getting Started with NextJS",
-  image: "getting-started-nextjs.png",
-  excerpt: "NextJS is a React Framework for Production",
-  date: "2023-12-18",
-  content: "# This is a first post",
-};
-
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+function PostContent(props) {
+  const { post } = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
